f1426418415587aadec201effde255c94959b76f
Samiran Das <dassamiran05@gmail.com>
2021-08-24 13:27:28 +0530

    Bolg website

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const responsive = {
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
+$(document).ready(function(){
+    $('.toggle-collapse').click(function(){
+        $('.nav').toggleClass('collapse');
+    });
+
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        responsive:responsive
+
+    });
+
+
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },2000);
+    });
+
+    AOS.init();
+});
